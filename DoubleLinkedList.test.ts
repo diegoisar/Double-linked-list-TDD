ab35a45263b44892389f083054162202036ebc03
diegoisar <diego.isar@lambdaloopers.com>
2024-08-11 12:33:03 +0200

    Test for TDD

--- DoubleLinkedList.test.ts
+++ DoubleLinkedList.test.ts
@@ -1,1 +1,56 @@
-describe('DoubleLinkedList', () => {})
+import { DoubleLinkedList } from './DoubleLinkedList';
+
+describe('DoubleLinkedList', () => {
+    let list: DoubleLinkedList<number>;
+
+    beforeEach(() => {
+        list = new DoubleLinkedList<number>();
+    });
+
+    test('should allow adding an element to the end', () => {
+        list.appendLast(3);
+        expect(list.getLast()?.value).toBe(3);
+        expect(list.size).toBe(1);
+    });
+
+    test('should allow adding an element to the beginning', () => {
+        list.appendFirst(3);
+        expect(list.getFirst()?.value).toBe(3);
+        expect(list.size).toBe(1);
+    });
+
+    test('should allow removing an element from the beginning', () => {
+        list.appendFirst(3);
+        list.appendFirst(4);
+        list.removeFirst();
+        expect(list.getFirst()?.value).toBe(3);
+        expect(list.size).toBe(1);
+    });
+
+    test('should allow removing an element from the end', () => {
+        list.appendLast(3);
+        list.appendLast(4);
+        list.removeLast();
+        expect(list.getLast()?.value).toBe(3);
+        expect(list.size).toBe(1);
+    });
+
+    test('should return the first element', () => {
+        list.appendFirst(3);
+        list.appendFirst(4);
+        expect(list.getFirst()?.value).toBe(4);
+    });
+
+    test('should return the last element', () => {
+        list.appendLast(3);
+        list.appendLast(4);
+        expect(list.getLast()?.value).toBe(4);
+    });
+
+    test('should return null if the list is empty', () => {
+        expect(list.getFirst()).toBeNull();
+        expect(list.getLast()).toBeNull();
+        expect(list.size).toBe(0);
+    });
+
+})
--- DoubleLinkedList.ts
+++ DoubleLinkedList.ts
@@ -1,1 +1,23 @@
-export class DoubleLinkedList{}
+class Node<T>{
+    value: T;
+    next: Node<T> | null = null;
+    prev: Node<T> | null = null;
+}
+
+export class DoubleLinkedList<T>{
+    public first: Node<T> | null = null;
+    public last: Node<T> | null = null;
+    public size: number = 0;
+
+    public appendFirst(value: T): void {}
+
+    public appendLast(value: T): void {}
+
+    public removeFirst(): void {}
+
+    public removeLast(): void {}
+
+    public getFirst(): Node<T>| null { return this.first; }
+
+    public getLast(): Node<T> | null { return this.last; }
+}
