44eb4fa6f0a25ff2692cb8114b651f46f4be7378
diegoisar <diego.isar@lambdaloopers.com>
2024-08-11 13:05:39 +0200

    Implementation and more test

--- DoubleLinkedList.test.ts
+++ DoubleLinkedList.test.ts
@@ -19,11 +19,22 @@ describe('DoubleLinkedList', () => {
         expect(list.size).toBe(1);
     });
 
-    test('should allow removing an element from the beginning', () => {
+    test('should allow adding multiple elements', () => {
         list.appendFirst(3);
-        list.appendFirst(4);
+        list.appendLast(4);
+        list.appendFirst(5);
+        expect(list.getFirst()?.value).toBe(5);
+        expect(list.getLast()?.value).toBe(4);
+        expect(list.size).toBe(3);
+        expect(list.getFirst()?.next?.value).toBe(3);
+        expect(list.getLast()?.prev?.value).toBe(3);
+    })
+
+    test('should allow removing an element from the beginning', () => {
+        list.appendLast(3);
+        list.appendLast(4);
         list.removeFirst();
-        expect(list.getFirst()?.value).toBe(3);
+        expect(list.getFirst()?.value).toBe(4);
         expect(list.size).toBe(1);
     });
 
@@ -52,5 +63,53 @@ describe('DoubleLinkedList', () => {
         expect(list.getLast()).toBeNull();
         expect(list.size).toBe(0);
     });
+
+    test('should be able to iterate all the element forward', () => {
+        for (let i = 0; i < 5; i++) {
+            list.appendLast(i);
+        }
+        const getValues = [];
+        let current = list.getFirst();
+        while(current){
+            getValues.push(current.value);
+            current = current.next;
+        }
+        expect(getValues).toEqual([0, 1, 2, 3, 4]);
+
+        for (let i = 0; i < 5; i++) {
+            list.appendFirst(i);
+        }
+        const getValues2 = [];
+        let current2 = list.getFirst();
+        while(current2){
+            getValues2.push(current2.value);
+            current2 = current2.next;
+        }
+        expect(getValues2).toEqual([4, 3, 2, 1, 0, 0, 1, 2, 3, 4]);
+    })
+
+    test ('should be able to iterate all the element backward', () => {
+        for (let i = 0; i < 5; i++) {
+            list.appendLast(i);
+        }
+        const getValues = [];
+        let current = list.getLast();
+        while(current){
+            getValues.push(current.value);
+            current = current.prev;
+        }
+        expect(getValues).toEqual([4, 3, 2, 1, 0]);
+
+        for (let i = 0; i < 5; i++) {
+            list.appendFirst(i);
+        }
+        const getValues2 = [];
+        let current2 = list.getLast();
+        while(current2){
+            getValues2.push(current2.value);
+            current2 = current2.prev;
+        }
+        expect(getValues2).toEqual([4, 3, 2, 1, 0, 0, 1, 2, 3, 4]);
+    })
 
 })
--- DoubleLinkedList.ts
+++ DoubleLinkedList.ts
@@ -1,21 +1,57 @@
 class Node<T>{
-    value: T;
-    next: Node<T> | null = null;
-    prev: Node<T> | null = null;
+    public value: T;
+    public next: Node<T> | null = null;
+    public prev: Node<T> | null = null;
+
+    constructor(value: T){
+        this.value = value;
+    }
 }
 
 export class DoubleLinkedList<T>{
-    public first: Node<T> | null = null;
-    public last: Node<T> | null = null;
+    private first: Node<T> | null = null;
+    private last: Node<T> | null = null;
     public size: number = 0;
 
-    public appendFirst(value: T): void {}
+    public appendFirst(value: T): void {
+        const newNode = new Node<T>(value);
+        if(this.first){
+            newNode.next = this.first;
+            this.first.prev = newNode;
+        }
+        else{
+            this.last = newNode;
+        }
+        this.first = newNode;
+        this.size++;
+    }
 
-    public appendLast(value: T): void {}
+    public appendLast(value: T): void {
+        const newNode = new Node<T>(value);
+        if(this.last){
+            newNode.prev = this.last;
+            this.last.next = newNode;
+        }
+        else{
+            this.first = newNode;
+        }
+        this.last = newNode;
+        this.size++;
+    }
 
-    public removeFirst(): void {}
+    public removeFirst(): void {
+        if(this.first){
+            this.first = this.first.next;
+            this.size--;
+        }
+    }
 
-    public removeLast(): void {}
+    public removeLast(): void {
+        if(this.last){
+            this.last = this.last.prev;
+            this.size--;
+        }
+    }
 
     public getFirst(): Node<T>| null { return this.first; }
 
